5b97708ab6a5fc02ecdd49c95bf7bf16c71467f0
Tiffany Madruga <tmadruga@fb.com>
2017-06-27 16:58:13 -0700

    Completed user stories to sign up, log in, log out, and persist user

--- instagram/AppDelegate.swift
+++ instagram/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
         Parse.initialize(
             with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
                 configuration.applicationId = "Instagram"
@@ -24,6 +25,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 configuration.server = "https://tranquil-reaches-19244.herokuapp.com/parse"
             })
         )
+        if PFUser.current() != nil {
+            print("Welcome Back!")
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let feedNavBar = storyboard.instantiateViewController(withIdentifier: "NavBar")
+            window?.rootViewController = feedNavBar
+            
+        }
         
         return true
     }
@@ -40,10 +49,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        print("hello")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+            
+            return true
+        }
+        
+        
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
---
+++ instagram/FeedViewController.swift
@@ -0,0 +1,48 @@
+//
+//  FeedViewController.swift
+//  instagram
+//
+//  Created by Tiffany Madruga on 6/27/17.
+//  Copyright © 2017 Tiffany Madruga. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func logOutButton(_ sender: Any) {
+        PFUser.logOutInBackground { (error: Error?) in
+            print("user has logged out")
+            
+            let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let userVC = mainStoryboard.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
+            self.present(userVC, animated: true, completion: nil)
+            
+        }
+        }
+        
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
